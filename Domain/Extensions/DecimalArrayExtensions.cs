04cbc1f9a2656321f5af9175fc724b03e0d7b2cb
Pierre G. Boutquin <pierre@boutquin.com>
2023-03-20 14:23:22 -0400

    Add project files.

---
+++ Domain/Extensions/DecimalArrayExtensions.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+using Boutquin.Domain.Exceptions;
+
+namespace Boutquin.Domain.Extensions;
+
+/// <summary>
+/// Provides extension methods for calculating statistical measures on an array of decimal values.
+/// </summary>
+public static class DecimalArrayExtensions
+{
+    /// <summary>
+    /// Enum to represent the calculation type for variance and standard deviation.
+    /// </summary>
+    public enum CalculationType
+    {
+        Sample,
+        Population
+    }
+
+    /// <summary>
+    /// Contains constants for exception messages.
+    /// </summary>
+    public static class ExceptionMessages
+    {
+        public const string EmptyOrNullArray = "Input array must not be empty or null.";
+        public const string InsufficientDataForSampleCalculation = "Input array must have at least two elements for sample calculation.";
+    }
+
+    /// <summary>
+    /// Custom exception for invalid input data.
+    /// </summary>
+    public class InvalidInputDataException : Exception
+    {
+        public InvalidInputDataException(string message) : base(message) { }
+    }
+
+    /// <summary>
+    /// Calculates the average of an array of decimal values.
+    /// </summary>
+    /// <param name="values">The array of decimal values.</param>
+    /// <returns>The average of the values.</returns>
+    /// <exception cref="InvalidInputDataException">Thrown when the input array is empty or null.</exception>
+    public static decimal Average(this decimal[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            throw new EmptyOrNullArrayException(ExceptionMessages.EmptyOrNullArray);
+        }
+
+        return values.Sum() / values.Length;
+    }
+
+    /// <summary>
+    /// Calculates the variance of an array of decimal values.
+    /// </summary>
+    /// <param name="values">The array of decimal values.</param>
+    /// <param name="calculationType">The type of calculation (sample or population).</param>
+    /// <returns>The variance of the values.</returns>
+    /// <exception cref="EmptyOrNullArrayException">Thrown when the input array is empty.</exception>
+    /// <exception cref="InsufficientDataException">Thrown when the input array contains less than two elements for sample calculation.</exception>
+    public static decimal Variance(this decimal[] values, CalculationType calculationType = CalculationType.Sample)
+    {
+        if (values == null || values.Length == 0)
+        {
+            throw new EmptyOrNullArrayException(ExceptionMessages.EmptyOrNullArray);
+        }
+
+        if (calculationType == CalculationType.Sample && values.Length == 1)
+        {
+            throw new InsufficientDataException(ExceptionMessages.InsufficientDataForSampleCalculation);
+        }
+
+        var avg = values.Average();
+        var sumOfSquares = values.Sum(x => (x - avg) * (x - avg));
+        var denominator = calculationType == CalculationType.Sample ? values.Length - 1 : values.Length;
+        return sumOfSquares / denominator;
+    }
+
+    /// <summary>
+    /// Calculates the standard deviation of an array of decimal values.
+    /// </summary>
+    /// <param name="values">The array of decimal values.</param>
+    /// <param name="calculationType">The type of calculation (sample or population).</param>
+    /// <returns>The standard deviation of the values.</returns>
+    /// <exception cref="EmptyOrNullArrayException">Thrown when the input array is empty.</exception>
+    /// <exception cref="InsufficientDataException">Thrown when the input array contains less than two elements for sample calculation.</exception>
+
+    public static decimal StandardDeviation(this decimal[] values, CalculationType calculationType = CalculationType.Sample)
+    {
+        return (decimal)Math.Sqrt((double)values.Variance(calculationType));
+    }
+}
---
+++ UnitTests/Domain/DecimalArrayExtensionsTests.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+using Boutquin.Domain.Exceptions;
+using Boutquin.Domain.Extensions;
+
+using static Boutquin.Domain.Extensions.DecimalArrayExtensions;
+
+namespace Boutquin.UnitTests.Domain;
+
+public class DecimalArrayExtensionsTests
+{
+    /// <summary>
+    /// Tests the Average method using test data provided by AverageData.
+    /// </summary>
+    [Theory]
+    [MemberData(nameof(DecimalArrayExtensionsTestData.AverageData), MemberType = typeof(DecimalArrayExtensionsTestData))]
+    public void Average_ShouldCalculateCorrectly(decimal[] values, decimal expected)
+    {
+        // Act
+        var result = values.Average();
+
+        // Assert
+        result.Should().BeApproximately(expected, 1e-12m);
+    }
+
+    /// <summary>
+    /// Tests the Variance method using test data provided by VarianceData.
+    /// </summary>
+    [Theory]
+    [MemberData(nameof(DecimalArrayExtensionsTestData.VarianceData), MemberType = typeof(DecimalArrayExtensionsTestData))]
+    public void Variance_ShouldCalculateCorrectly(decimal[] values, CalculationType calculationType, decimal expected)
+    {
+        // Act
+        var result = values.Variance(calculationType);
+
+        // Assert
+        result.Should().BeApproximately(expected, 1e-12m);
+    }
+
+    /// <summary>
+    /// Tests the StandardDeviation method using test data provided by StandardDeviationData.
+    /// </summary>
+    [Theory]
+    [MemberData(nameof(DecimalArrayExtensionsTestData.StandardDeviationData), MemberType = typeof(DecimalArrayExtensionsTestData))]
+    public void StandardDeviation_ShouldCalculateCorrectly(decimal[] values, CalculationType calculationType, decimal expected)
+    {
+        // Act
+        var result = values.StandardDeviation(calculationType);
+
+        // Assert
+        result.Should().BeApproximately(expected, 1e-12m);
+    }
+
+    /// <summary>
+    /// Tests the InsufficientDataForSampleCalculation for all extension methods that require sample calculations with an input array containing only one element.
+    /// </summary>
+    [Fact]
+    public void AllMethods_ShouldThrowInsufficientDataForSampleCalculation_WhenArrayHasOneElement()
+    {
+        var values = new decimal[] { 0.01m };
+        var exceptionType = typeof(InsufficientDataException);
+        var exceptionMessage = ExceptionMessages.InsufficientDataForSampleCalculation;
+
+        Assert.Throws(exceptionType, () => values.Variance(CalculationType.Sample)).Message.Should().Be(exceptionMessage);
+        Assert.Throws(exceptionType, () => values.StandardDeviation(CalculationType.Sample)).Message.Should().Be(exceptionMessage);
+    }
+
+    /// <summary>
+    /// Tests the EmptyOrNullArrayException for all extension methods with null input arrays.
+    /// </summary>
+    [Fact]
+    public void AllMethods_ShouldThrowEmptyOrNullArrayException_WhenArrayIsNull()
+    {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        decimal[] values = null;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        var exceptionType = typeof(EmptyOrNullArrayException);
+        var exceptionMessage = ExceptionMessages.EmptyOrNullArray;
+
+#pragma warning disable CS8604 // Possible null reference argument.
+        Assert.Throws(exceptionType, () => values.Average()).Message.Should().Be(exceptionMessage);
+        Assert.Throws(exceptionType, () => values.Variance()).Message.Should().Be(exceptionMessage);
+        Assert.Throws(exceptionType, () => values.StandardDeviation()).Message.Should().Be(exceptionMessage);
+#pragma warning restore CS8604 // Possible null reference argument.
+    }
+
+    /// <summary>
+    /// Tests the EmptyOrNullArrayException for all extension methods with empty input arrays.
+    /// </summary>
+    [Fact]
+    public void AllMethods_ShouldThrowEmptyOrNullArrayException_WhenArrayIsEmpty()
+    {
+        var values = Array.Empty<decimal>();
+        var exceptionType = typeof(EmptyOrNullArrayException);
+        var exceptionMessage = ExceptionMessages.EmptyOrNullArray;
+
+        Assert.Throws(exceptionType, () => values.Average()).Message.Should().Be(exceptionMessage);
+        Assert.Throws(exceptionType, () => values.Variance()).Message.Should().Be(exceptionMessage);
+        Assert.Throws(exceptionType, () => values.StandardDeviation()).Message.Should().Be(exceptionMessage);
+    }
+}
