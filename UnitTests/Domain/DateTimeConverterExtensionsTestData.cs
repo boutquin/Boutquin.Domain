679b3c5b89254c29c3f83ae833bf238862c3a49f
Pierre G. Boutquin <pierre@boutquin.com>
2023-03-30 20:10:12 -0400

    Added DateTimeExtensions with Unit Tests

---
+++ UnitTests/Domain/DateTimeConverterExtensionsTestData.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+using System.Collections.Generic;
+
+namespace Boutquin.UnitTests.Domain;
+
+/// <summary>
+/// Test data for the DateTimeConverterExtensionsTests class.
+/// </summary>
+public static class DateTimeConverterExtensionsTestData
+{
+    public static IEnumerable<object[]> ConvertTimeZoneCases =>
+        new List<object[]>
+        {
+            // Test case 1: Normal case
+            new object[] { new DateTime(2023, 3, 28, 12, 0, 0), "UTC", "America/New_York", new DateTime(2023, 3, 28, 8, 0, 0) },
+
+            // Test case 2: Another normal case
+            new object[] { new DateTime(2023, 3, 28, 12, 0, 0), "UTC", "Asia/Tokyo", new DateTime(2023, 3, 28, 21, 0, 0) },
+
+            // Test case 3: Same source and target time zones
+            new object[] { new DateTime(2023, 3, 28, 12, 0, 0), "UTC", "UTC", new DateTime(2023, 3, 28, 12, 0, 0) },
+
+            // Test case 4: Daylight saving time transition
+            new object[] { new DateTime(2023, 3, 10, 2, 0, 0), "America/New_York", "UTC", new DateTime(2023, 3, 10, 7, 0, 0) }
+        };
+}
---
+++ UnitTests/Domain/DateTimeConverterExtensionsTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+using Boutquin.Domain.Extensions;
+
+namespace Boutquin.UnitTests.Domain;
+
+public sealed class DateTimeConverterExtensionsTests
+{
+    /// <summary>
+    /// Test cases for the ConvertTimeZone method.
+    /// </summary>
+    /// <param name="sourceDateTime">The source date/time.</param>
+    /// <param name="sourceTimeZoneId">The source time zone ID.</param>
+    /// <param name="targetTimeZoneId">The target time zone ID.</param>
+    /// <param name="expectedDateTime">The expected date/time after conversion.</param>
+    [Theory]
+    [MemberData(nameof(DateTimeConverterExtensionsTestData.ConvertTimeZoneCases), MemberType = typeof(DateTimeConverterExtensionsTestData))]
+    public void ConvertTimeZone_ShouldReturnCorrectResult(DateTime sourceDateTime, string sourceTimeZoneId, string targetTimeZoneId, DateTime expectedDateTime)
+    {
+        // Act
+        DateTime actualDateTime = sourceDateTime.ConvertTimeZone(sourceTimeZoneId, targetTimeZoneId);
+
+        // Assert
+        actualDateTime.Should().Be(expectedDateTime);
+    }
+
+    /// <summary>
+    /// Test case for the ConvertTimeZone method when the targetTimeZoneId is null.
+    /// </summary>
+    [Fact]
+    public void ConvertTimeZone_ShouldThrowArgumentNullException_WhenTargetTimeZoneIdIsNull()
+    {
+        // Arrange
+        var sourceDateTime = new DateTime(2023, 3, 28, 12, 0, 0);
+        var sourceTimeZoneId = "UTC";
+        string targetTimeZoneId = null;
+
+        // Act and Assert
+        Assert.Throws<ArgumentNullException>(() => sourceDateTime.ConvertTimeZone(sourceTimeZoneId, targetTimeZoneId));
+    }
+
+    /// <summary>
+    /// Test case for the ConvertTimeZone method when the sourceTimeZoneId is not a valid time zone identifier.
+    /// </summary>
+    [Fact]
+    public void ConvertTimeZone_ShouldThrowTimeZoneNotFoundException_WhenSourceTimeZoneIdIsInvalid()
+    {
+        // Arrange
+        var sourceDateTime = new DateTime(2023, 3, 28, 12, 0, 0);
+        var sourceTimeZoneId = "Invalid/TimeZone";
+        var targetTimeZoneId = "UTC";
+
+        // Act and Assert
+        Assert.Throws<TimeZoneNotFoundException>(() => sourceDateTime.ConvertTimeZone(sourceTimeZoneId, targetTimeZoneId));
+    }
+
+    /// <summary>
+    /// Test case for the ConvertTimeZone method when the targetTimeZoneId is not a valid time zone identifier.
+    /// </summary>
+    [Fact]
+    public void ConvertTimeZone_ShouldThrowTimeZoneNotFoundException_WhenTargetTimeZoneIdIsInvalid()
+    {
+        // Arrange
+        var sourceDateTime = new DateTime(2023, 3, 28, 12, 0, 0);
+        var sourceTimeZoneId = "UTC";
+        var targetTimeZoneId = "Invalid/TimeZone";
+
+        // Act and Assert
+        Assert.Throws<TimeZoneNotFoundException>(() => sourceDateTime.ConvertTimeZone(sourceTimeZoneId, targetTimeZoneId));
+    }
+}
