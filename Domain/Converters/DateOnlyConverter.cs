dfd80c4f2f9c52d2f7b97b5504745632547ad093
Pierre G. Boutquin <pierre@boutquin.com>
2023-04-30 20:15:41 -0400

    Add DateOnlyConverter

---
+++ Domain/Converters/DateOnlyConverter.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+namespace Boutquin.Domain.Converters;
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// A custom JSON converter for serializing and deserializing DateOnly objects.
+/// </summary>
+public sealed class DateOnlyConverter : JsonConverter<DateOnly>
+{
+    private const string DateFormat = "yyyy-MM-dd";
+
+    /// <summary>
+    /// Deserializes a JSON string to a DateOnly object.
+    /// </summary>
+    /// <param name="reader">The JSON reader.</param>
+    /// <param name="typeToConvert">The type of the object being deserialized.</param>
+    /// <param name="options">The JSON serializer options.</param>
+    /// <returns>A DateOnly object deserialized from the JSON string.</returns>
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.Parse(reader.GetString());
+    }
+
+    /// <summary>
+    /// Serializes a DateOnly object to a JSON string.
+    /// </summary>
+    /// <param name="writer">The JSON writer.</param>
+    /// <param name="value">The DateOnly object to serialize.</param>
+    /// <param name="options">The JSON serializer options.</param>
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat));
+    }
+}
---
+++ Domain/Extensions/JsonElementExtensions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2023 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+namespace Boutquin.Domain.Extensions;
+
+using System.Buffers;
+using System.Text.Json;
+
+/// <summary>
+/// Provides extension methods for the System.Text.Json.JsonElement class.
+/// </summary>
+public static class JsonElementExtensions
+{
+    /// <summary>
+    /// Deserializes the given JsonElement to an object of type T using the specified JsonSerializerOptions.
+    /// </summary>
+    /// <typeparam name="T">The target type of the deserialization.</typeparam>
+    /// <param name="element">The JsonElement to deserialize.</param>
+    /// <param name="options">Optional JsonSerializerOptions to use during deserialization. If not provided, the default options are used.</param>
+    /// <returns>An object of type T created from the given JsonElement.</returns>
+    /// <example>
+    /// <code>
+    /// JsonElement element = ...;
+    /// MyClass obj = element.ToObject&lt;MyClass&gt;();
+    /// </code>
+    /// </example>
+    public static T? ToObject<T>(this JsonElement element, JsonSerializerOptions options = null)
+    {
+        var bufferWriter = new ArrayBufferWriter<byte>();
+        using (var writer = new Utf8JsonWriter(bufferWriter))
+        {
+            element.WriteTo(writer);
+        }
+        return JsonSerializer.Deserialize<T>(bufferWriter.WrittenSpan, options);
+    }
+}
