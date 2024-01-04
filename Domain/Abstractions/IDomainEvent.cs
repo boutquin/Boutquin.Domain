078926128eaa6f71f03d4c78c3ed274f3d2024ee
Pierre G. Boutquin <pierre@boutquin.com>
2024-01-03 20:00:01 -0500

    Moved events into Domain.

--- Domain/Abstractions/IDomainEvent.cs
+++ Domain/Abstractions/IDomainEvent.cs
@@ -12,10 +12,9 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
+namespace Boutquin.Domain.Abstractions;
 
 using MediatR;
-
-namespace Boutquin.Events.Abstractions;
 
 /// <summary>
 /// Represents a domain event within the system.
