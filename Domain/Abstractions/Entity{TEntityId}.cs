1ddce8815b62968e4943a9c295f19025b9dca10f
Pierre G. Boutquin <pierre@boutquin.com>
2024-01-03 20:00:01 -0500

    Moven events into Domain.

--- Domain/Abstractions/Entity{TEntityId}.cs
+++ Domain/Abstractions/Entity{TEntityId}.cs
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-namespace Boutquin.Events.Abstractions;
+namespace Boutquin.Domain.Abstractions;
 
 /// <summary>
 /// Represents a base class for entities in the domain model, with a unique identifier and domain event handling capabilities.
--- Domain/Abstractions/IEntity.cs
+++ Domain/Abstractions/IEntity.cs
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-namespace Boutquin.Events.Abstractions;
+namespace Boutquin.Domain.Abstractions;
 
 /// <summary>
 /// Represents a domain entity that can generate and store domain events.
